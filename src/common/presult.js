35813bd7a92092b91281e3dbc4514aeaaee6ced8
Gil Tayar <gil@tayar.org>
2021-11-06 10:21:45 +0200

    use async/await in presult to be easier to understand

--- src/common/presult.js
+++ src/common/presult.js
@@ -1,11 +1,31 @@
 /**
  * @template T
+ * @template TErr
  * @param {Promise<T>} promise
- * @returns {Promise<[undefined, T] | [Error, undefined]>}
+ * @returns {Promise<[undefined, T] | [TErr, undefined]>}
  */
-export function presult(promise) {
-  return promise.then(
-    (v) => [undefined, v],
-    (e) => [e, undefined],
-  )
+export async function presult(promise) {
+  try {
+    const value = await promise
+
+    return [undefined, value]
+  } catch (/** @type {any} */ err) {
+    return [err, undefined]
+  }
+}
+
+/**
+ * @template T
+ * @template TErr
+ * @param {Promise<[err: TErr | undefined, value: T | undefined]>} presultPromise
+ * @returns {Promise<T | Promise<never>>}
+ */
+export async function unwrapPresult(presultPromise) {
+  const [err, value] = await presultPromise
+
+  if (err) {
+    throw err
+  } else {
+    return /** @type {T} */ (value)
+  }
 }
